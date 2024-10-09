862eac02e9aa0ca4bd0db8532a1a4edda8c6face
otteru <otteru@naver.com>
2024-10-09 19:42:01 +0900

    section 10 complete

--- src/main/java/hello/exception/api/ApiExceptionV2Controller.java
+++ src/main/java/hello/exception/api/ApiExceptionV2Controller.java
@@ -13,29 +13,8 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 public class ApiExceptionV2Controller {
 
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    @ExceptionHandler(IllegalArgumentException.class)
-    public ErrorResult illegalExHandler(IllegalArgumentException e) {
-        log.error("[exceptionHandler] ex", e);
-        return new ErrorResult("BAD", e.getMessage());
-    }
-
-    @ExceptionHandler
-    public ResponseEntity<ErrorResult> userExHandler(UserException e) {
-        log.error("[exceptionHandler] ex", e);
-        ErrorResult errorResult = new ErrorResult("USER-EX", e.getMessage());
-        return new ResponseEntity(errorResult, HttpStatus.BAD_REQUEST);
-    }
-
-    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    @ExceptionHandler
-    public ErrorResult exHandler(Exception e) {
-        log.error("[exceptionHandler] ex", e);
-        return new ErrorResult("EX", "내부 오류");
-    }
-
     @GetMapping("/api2/members/{id}")
-    public ApiExceptionController.MemberDto getMember(@PathVariable("id") String id){
+    public MemberDto getMember(@PathVariable("id") String id){
 
         if(id.equals("ex")) {
             throw new RuntimeException("잘못된 사용자");
@@ -47,7 +26,7 @@ public class ApiExceptionV2Controller {
             throw new UserException("사용자 오류");
         }
 
-        return new ApiExceptionController.MemberDto(id, "hello"+id);
+        return new MemberDto(id, "hello"+id);
     }
 
     @Data
